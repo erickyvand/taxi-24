01d444e725752635c35a3da4839c8bceb7498870
Ericky Vand <erickyvand@gmail.com>
2020-10-01 23:14:08 +0200

    specific driver

---
+++ src/validations/driver.validation.js
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+import ResponseService from '../services/response.service';
+
+export const validateDriverUrlParam = (req, res, next) => {
+	const schema = Joi.object({
+		driverId: Joi.string()
+			.regex(/^[0-9]{1,}$/)
+			.messages({
+				'string.pattern.base': 'Id must be a number',
+			}),
+	}).options({ abortEarly: false });
+
+	const { error } = schema.validate(req.params);
+
+	if (error) {
+		const errors = error.details.map(error => error.message);
+		ResponseService.setError(400, errors);
+		return ResponseService.send(res);
+	}
+	next();
+};
