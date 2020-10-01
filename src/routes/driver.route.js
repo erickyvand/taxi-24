057f452b05a6c1651f880c709da9ded4eb194cfe
Ericky Vand <erickyvand@gmail.com>
2020-10-01 21:16:48 +0200

    get available drivers

--- src/routes/driver.route.js
+++ src/routes/driver.route.js
@@ -4,5 +4,6 @@ import DriverController from '../controllers/driver.controller';
 const router = express.Router();
 
 router.get('/', DriverController.getAllDrivers);
+router.get('/available', DriverController.getAvailableDrivers)
 
 export default router;
