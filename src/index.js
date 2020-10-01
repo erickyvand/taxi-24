a5da2ba936b1d06850fc1cd8e3640bd1aec0c355
Ericky Vand <erickyvand@gmail.com>
2020-10-01 18:09:59 +0200

    project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+} )
